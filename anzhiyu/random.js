8de0d3b4d85a80a7c9de6764dbd6273e5351e182
solitarybamboo <bamboo05501235@gmail.com>
2025-03-28 01:50:03 +0800

    Site updated: 2025-03-28 01:50:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/27/hello-world/"];function toRandomPost(){
+var posts=["2025/03/27/hello-world/","2025/03/28/算法模板/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
